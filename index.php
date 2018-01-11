3af99d1130d9a5a3df2d67310638cd0578d21abd
Dracologist <ejckanzler@yahoo.com>
2018-01-11 16:43:50 -0500

    Added index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: Elizabeth Kanzler
+     * Date: 1/11/2018
+     * Time: 1:36 PM
+     */ ?>
+
+
+    <head>
+        <meta charset="UTF-8">
+        <title>JavaScript Exercises</title>
+    </head>
+
+    <body>
+        <h1>Hello, World!</h1>
+    </body>
+</html>
